bef77af41a9c5811806da1e4eb4ae7328fcb9669
Asmit-Yadav07 <suspiciousfun987@gmail.com>
2026-07-28 16:29:05 +0530

    Added NavBar

---
+++ FRONTEND/Components/Header.tsx
@@ -0,0 +1,43 @@
+import { HugeiconsIcon } from "@hugeicons/react";
+import { Linkedin01Icon, GithubIcon } from "@hugeicons/core-free-icons";
+
+function Header() {
+  return (
+    <>
+      <div className="flex justify-around items-center pt-5  gap-15 ">
+        <div className="h-14 w-20 text-center drop-shadow-[0_0_10px_#39D9FF]">
+          <p className="font-[Orbitron] text-[20px] text-white">ASMIT </p>
+          <p className="font-[Orbitron] text-[10px] text-[#39D9FF] ">
+            PORTFOLIO
+          </p>
+        </div>
+        <div className="flex font-[Orbitron] text-white gap-7 text-[12px] font-bold  bg-white/10  backdrop-blur-xs w-[45vw] h-[8vh]  rounded-4xl  items-center justify-center border border-[#4c3cff]/30">
+          <ul>HOME</ul>
+          <ul>ABOUT</ul>
+          <ul>PROJECTS</ul>
+          <ul>SKILLS</ul>
+          <ul>EXPERIENCE</ul>
+          <ul>CONTACT</ul>
+        </div>
+        <div className="flex gap-3 justify-center items-center">
+          <HugeiconsIcon
+            icon={Linkedin01Icon}
+            size={30}
+            color="#ffffff"
+            strokeWidth={1.5}
+            className=" w-12 h-12 p-2 rounded-xl  bg-[#050816]/60 backdrop-blur-md border border-[#4c3cff]/30 shadow-[0_0_15px_rgba(76,60,255,0.25)]"
+          />
+          <HugeiconsIcon
+            icon={GithubIcon}
+            size={30}
+            color="#ffffff"
+            strokeWidth={1.5}
+            className=" w-12 h-12 p-2 rounded-xl  bg-[#050816]/60 backdrop-blur-md border border-[#4c3cff]/30 shadow-[0_0_15px_rgba(76,60,255,0.25)]"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -1,10 +1,39 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
+import Header from "../Components/Header.js";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      {/* <div className="relative w-screen h-screen overflow-hidden z-50">
+
+        <img
+          src="/Background.png"
+          alt="Background"
+          className="absolute inset-0 w-full h-full object-cover"
+        />
+
+   
+        <img
+          src="../public/Charactercopy.png"
+          alt="Character"
+          className="absolute left-1/2 -translate-x-1/2  top-3 h-[80vh] object-contain z-50"
+        />
+        <div className="absolute inset-0   bg-white/10 z-10" />
+      </div> */}
+      <div className="relative w-screen h-screen overflow-hidden z-50">
+        <img
+          src="../public/WholeBackground.png"
+          className="absolute inset-0 w-full h-full object-cover brightness-80"
+        ></img>
+        <div className="absolute inset-0 z-45">
+          <Header />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
